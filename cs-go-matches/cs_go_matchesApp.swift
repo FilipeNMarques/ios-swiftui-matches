1cf66d4335e4c617cc415326ac28968f6faec131
Filipe Marques <filipenmarques1@gmail.com>
2024-09-02 10:11:24 -0300

    Initial Commit

---
+++ cs-go-matches/cs_go_matchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs_go_matchesApp.swift
+//  cs-go-matches
+//
+//  Created by Filipe Marques on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct cs_go_matchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
